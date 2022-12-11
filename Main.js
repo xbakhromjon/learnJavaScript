693631d9bbbc00373f37262e2a50e6cab4f35b2a
xbakhromjon <xbakhromjon@gmail.com>
2022-12-11 17:22:59 +0500

    init

---
+++ Main.js
@@ -0,0 +1,39 @@
+// alert('Hello, world!');
+//
+// alert(3 +
+//     1
+//     + 2);
+
+// alert("some code");
+// // "use strict" below is ignored--it must be at the top
+//
+// "use strict";
+
+// strict mode is not activated
+
+let message
+message = "JavaScript"
+console.log(message)
+
+message = 'Java';
+
+message = 12;
+console.log(message);
+
+let a, b, c
+const COLOR_RED = "#F00";
+
+
+console.info(1 / 0);
+console.info("a" / 2);
+let bigInt = 900719925474099523123n;
+console.log(bigInt); // 9007199254740992
+
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+console.log(`${1 + 2}`)
+
+
+
+
