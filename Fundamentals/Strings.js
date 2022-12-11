0414dbe6695e49f7dc37032f3ed2ffe04b91c4b7
xbakhromjon <xbakhromjon@gmail.com>
2022-12-11 17:26:11 +0500

    init

---
+++ Fundamentals/Strings.js
@@ -0,0 +1,4 @@
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+console.log(`${1 + 2}`)
---
+++ Fundamentals/Variables.js
@@ -0,0 +1,25 @@
+let message;
+var message1;
+const COLOR_RED = "#F00";
+
+message = "JavaScript"
+
+console.log(message)
+
+message = 'Java';
+message = 12;
+
+console.log(message);
+let a, b, c
+
+
+console.info(1 / 0);
+console.info("a" / 2);
+let bigInt = 900719925474099523123n;
+console.log(bigInt); // 9007199254740992
+
+
+
+
+
+
